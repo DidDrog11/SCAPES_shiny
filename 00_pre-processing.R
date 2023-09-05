7032a98efcf06bf73b9de6d8e388bc52dad8b986
David Simons <dsimons1987@gmail.com>
2023-09-05 10:48:43 -0700

    first commit

---
+++ 00_pre-processing.R
@@ -0,0 +1,88 @@
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(readr)) install.packages("readr", repos = "http://cran.us.r-project.org")
+if(!require(leaflet)) install.packages("leaflet", repos = "http://cran.us.r-project.org")
+if(!require(leaflet.extras)) install.packages("leaflet.extras", repos = "http://cran.us.r-project.org")
+if(!require(terra)) install.packages("terra", repos = "http://cran.us.r-project.org")
+if(!require(sf)) install.packages("sf", repos = "http://cran.us.r-project.org")
+if(!require(here)) install.packages("here", repos = "http://cran.us.r-project.org")
+if(!require(DT)) install.packages("DT", repos = "http://cran.us.r-project.org")
+if(!require(geodata)) install.packages("geodata", repos = "http://cran.us.r-project.org")
+
+library(tidyverse)
+library(readr)
+library(leaflet)
+library(leaflet.extras)
+library(terra)
+library(sf)
+library(here)
+library(DT)
+library(geodata)
+
+sites_raw <- read_csv(here("www", "sites_unfiltered_2km_2023-08-30.csv"))
+
+column_order <- c("village_id", "village", "source", "longitude", "latitude", "lassa_occ", "num_buildings", "treecover_tot_area", "state", "LGA", "SenDist",
+                  "shrubland_tot_area", "grassland_tot_area", "builtup_tot_area", "cropland_tot_area",
+                  "treecover_patch_area", "shrubland_patch_area", "grassland_patch_area", "builtup_patch_area",  "cropland_patch_area",
+                  "landscape_patch_area", "landscape_area_sd", 
+                  "treecover_percent", "shrubland_percent", "grassland_percent", "builtup_percent", "cropland_percent",
+                  "travel_time", "distance_to_road", "build_area")
+
+sites_cleaned <- sites_raw %>%
+  mutate(across(contains("percent"), \(x) round(x, digits = 2)),
+         across(contains("area"), \(x) round(x, digits = 4)),
+         build_area = round(build_area, 0),
+         lassa_occ = round(lassa_occ, 4)) %>%
+  select(all_of(column_order))
+
+# Previously selected groups and clustering
+current_groups <- bind_rows(
+  tibble(group = rep(1, 9),
+         cluster = c(rep("1A", 3), rep("1B", 4), rep("1C", 2)),
+         village = c(c("Eja", "Ogamana", "Okunbongha"), c("Ndiweya", "Manden", "Akudfune", "Mgbumgangbom"), c("Abayono", "Abaribara"))),
+  tibble(group = rep(2, 41),
+         cluster = c(rep("2D", 7), rep("2E", 17), rep("2F", 7), rep("2I", 7), rep("2J", 3)),
+         village = c(c("Shingile", "Wanagina", "Adum", "Anyogbe", "Nfom", "Owoleyeche", "Iduku"), 
+                     c("Igberi", "Umuta", "Ikwo", "Opuitumo", "Nsobo", "Afiaelem", "Nko", "Eka", "Aru", "Ndilche Achara", "Agbanyim", "Otabo", "Mbode",
+                       "Ebiom", "Akpanudele", "Udomowo", "Ohike"),
+                     c("Ijibollo", "Apiapum Eja", "Obagu Ete", "Okum", "Ovoaba", "Azu Anyim", "Ijutum"),
+                     c("Ndingele", "Olua", "Ominiyi", "Ominyi", "Amaguanyim", "Ndowa", "Ndowu"),
+                     c("Ofianko", "Ndiogbokote", "Okponga"))),
+  tibble(group = rep(3, 30),
+         cluster = c(rep("3G", 6), rep("3H", 6), rep("3K", 4), rep("3L", 4), rep("3M", 5), rep("3N", 5)),
+         village = c(c("Ugbala", "Ozante", "Ndiawala", "Izenyi", "Offianwe", "Isi Ohumini"),
+                     c("Mkpumeakwaokoro", "Edenyaka", "Inyinba", "Ndibinaofia", "Ndibiofa", "Okpoduma"),
+                     c("Ijilaga", "Ijibollo", "Odariko", "Ubeagu Ndikuda"),
+                     c("Enyibichiri", "Elekpe", "Ndiofutu Echara", "Mgbabu"),
+                     c("Okwaraka", "Ndialagu Akpu", "Akpu", "Agbara Oza", "Ndibulofia"),
+                     c("Indingele", "Indiogogo", "Indigele", "Nwomila", "Indinwikwe")))
+  ) %>%
+  arrange(group, cluster, village) %>%
+  mutate(cluster = fct_inorder(cluster),
+         group = fct_inorder(as.character(group)))
+
+# Information on Lassa seroprevalence
+# Unsure how accurate these name matches are as there are some spelling differences but closest I could find
+
+lassa_sero <- tibble(village = c(c("Okpuitomo", "Abakaliki", "Abofifia", "Ndiawala", "Agbaja", "Offianwe", "Ndiofutu"),
+                                 c("Isieke", "Agalagu", "Amagu")),
+                     lassa_seroprevalence = c(rep("High", 7),
+                                              rep("Medium", 3))) %>%
+  mutate(lassa_seroprevalence = factor(lassa_seroprevalence, levels = c("Medium", "High")))
+                                              
+# Bring this additional data into the base site data
+
+sites_final <- sites_cleaned %>%
+  left_join(current_groups) %>%
+  left_join(lassa_sero) %>%
+  filter(!(village == "Ijibollo" & treecover_percent == 0.24 & cluster == "2F")) %>%
+  filter(!(village == "Ijibollo" & treecover_percent == 0.18 & cluster == "3K")) %>% # This village name is duplicated, remove the wrongly allocated cluster manually
+  mutate(across(where(is.numeric), ~replace_na(.x, 0)),
+         distance_to_road = distance_to_road/1000)
+
+write_rds(sites_final, here("www", "sites_final.rds"))
+
+# Get Nigeria shapefiles
+
+NGA_0 <- gadm("NGA", level = 0, path = here("www"))
+NGA_1 <- gadm("NGA", level = 1, path = here("www"))
+NGA_2 <- gadm("NGA", level = 2, path = here("www"))
---
+++ app.R
@@ -0,0 +1,278 @@
+if(!require(shiny)) install.packages("shiny", repos = "http://cran.us.r-project.org")
+if(!require(shinydashboard)) install.packages("shinydashboard", repos = "http://cran.us.r-project.org")
+if(!require(shinyWidgets)) install.packages("shinyWidgets", repos = "http://cran.us.r-project.org")
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(cowplot)) install.packages("cowplot", repos = "http://cran.us.r-project.org")
+if(!require(readr)) install.packages("readr", repos = "http://cran.us.r-project.org")
+if(!require(leaflet)) install.packages("leaflet", repos = "http://cran.us.r-project.org")
+if(!require(leaflet.extras)) install.packages("leaflet.extras", repos = "http://cran.us.r-project.org")
+if(!require(ggiraph)) install.packages("ggiraph", repos = "http://cran.us.r-project.org")
+if(!require(terra)) install.packages("terra", repos = "http://cran.us.r-project.org")
+if(!require(sf)) install.packages("sf", repos = "http://cran.us.r-project.org")
+if(!require(here)) install.packages("here", repos = "http://cran.us.r-project.org")
+if(!require(DT)) install.packages("DT", repos = "http://cran.us.r-project.org")
+
+library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
+library(tidyverse)
+library(cowplot)
+library(readr)
+library(leaflet)
+library(leaflet.extras)
+library(ggiraph)
+library(terra)
+library(sf)
+library(here)
+library(DT)
+
+# Data has been produced in an associated but separate repository
+# Data is stored in /www and there is an associated R scrip 00_pre-processing.R that does some final cleaning outside of the app
+site_df <- read_rds(here("www", "sites_final.rds")) %>%
+    st_as_sf(coords = c("longitude", "latitude"),
+             crs = "EPSG:4326")
+lc <- rast(here("www", "ds_processed_raster.tif"))
+
+adm_0 <- vect(here("www", "gadm", "gadm41_NGA_0_pk.rds"))
+adm_1 <- vect(here("www", "gadm", "gadm41_NGA_1_pk.rds"))
+adm_2 <- vect(here("www", "gadm", "gadm41_NGA_2_pk.rds"))
+
+# Define UI for application that draws a histogram
+ui <- dashboardPage(
+    
+    skin = "purple",
+    
+    dashboardHeader(title = "SCAPES Nigeria",
+                    titleWidth = 450),
+    
+    dashboardSidebar(
+        width = 350,
+        sidebarMenu(
+            menuItem("Homepage", tabName = "homepage", icon = icon("passport")),
+            menuItem("Site selection", icon = icon("house"),
+                     menuSubItem("Potential village sites", tabName = "village_sites"),
+                     menuSubItem("Group 1", tabName = "group_1"))
+        )
+    ),
+    
+    
+    ## Homepage ----------------------------------------------------------------
+    
+    dashboardBody(
+        tabItems(
+            # Homepage tab content
+            tabItem(tabName = "homepage",
+                    fluidRow(
+                        
+                        box(width = 12,
+                            h1("Introduction"),
+                            p(HTML("This is a shiny app for several tasks and visualisations within the <a href=`https://www.saganfriant.com/risk-lab`>SCAPES</a> project.")),
+                            p("The Site selection tab contains several visualisations and methods to identify the housing clusters included in the study. The first page, `Potential village sites` shows all of the identified villages in the study region. Data on village locations were obtained from the OpenStreetMap (OSM) project and the Geographic Names Server (GNS)."),
+                            p(HTML("Within the `Potential village sites` tab filters can be used to show a subset of the returned villages. These filters include measures of the `Travel time to Abakaliki` in minutes (obtained from OSM), `Distance to main road` in km (obtained from OSM), the `number of buildings` (obtained from Google open data), and measures of proportion of different land use (from the ESA). Finally, the probability of Lassa occurrence has been estimated from a model produced by Redding et al. (<a href = 'https://www.nature.com/articles/s41467-021-25910-y'>link</a>). Sites can be filtered using these characteristics.")),
+                            p("The basemap for the `Potential village sites` map can be switched between OSM and a satellite image. The landuse raster produced from the ESA image can be overlain to the area to visualise recorded landuse for the pixel. ")
+                        )
+                    )),
+            
+            
+            ## Village sites ---------------------------------------------------------
+            
+            tabItem(tabName = "village_sites",
+                    position = "left",
+                                  sidebarPanel(
+                                      sliderInput(inputId = "TravTime",
+                                                  label = "Travel Time to Abakaliki (mins)",
+                                                  min = 0,
+                                                  step= 10,
+                                                  max = 300,
+                                                  value = c(0,150)),
+                                      sliderInput(inputId = "RoadDist",
+                                                  label = "Distance to main road (km)",
+                                                  min = 0,
+                                                  step= 0.5,
+                                                  max = 50,
+                                                  value = c(0,50)),
+                                      sliderInput(inputId = "NumBuild",
+                                                  label = "Number of buildings",
+                                                  min = 0,
+                                                  step= 50,
+                                                  max = 18000,
+                                                  value = c(0,1500)),
+                                      sliderInput(inputId = "BuiltUpSelect", 
+                                                  label = "Proportion Built-up", 
+                                                  min = 0,
+                                                  step = 0.05,
+                                                  max = 1,
+                                                  value = c(0,1)), 
+                                      sliderInput(inputId = "TreeSelect", 
+                                                  label = "Proportion Treecover", 
+                                                  min = 0,
+                                                  step = 0.05,
+                                                  max = 1,
+                                                  value = c(0,1)), 
+                                      sliderInput(inputId = "CropSelect", 
+                                                  label = "Proportion Cropland", 
+                                                  min = 0,
+                                                  step=0.05,
+                                                  max = 1,
+                                                  value = c(0,1)), 
+                                      sliderInput(inputId = "GrassSelect", 
+                                                  label = "Proportion Grassland", 
+                                                  min = 0,
+                                                  max = 1,
+                                                  step=0.05,
+                                                  value = c(0,1)),
+                                      sliderInput(inputId = "ShrubSelect", 
+                                                  label = "Proportion Shrubland", 
+                                                  min = 0,
+                                                  max = 1,
+                                                  step=0.05,
+                                                  value = c(0,1)),
+                                      sliderInput(inputId = "LassaSelect", 
+                                                  label = "Lassa occurrence probability", 
+                                                  min = 0,
+                                                  max = 1,
+                                                  step=0.05,
+                                                  value = c(0,1))
+                                  ),
+                    mainPanel(
+                        
+                        leafletOutput("sitemap", height = "850px")
+                        
+                    )),
+            
+            tabItem(tabName = "group_1",
+                    fluidRow(
+                        box(width = 12,
+                            h1("Group 1"),
+                            p("Group 1 villages are those with a central nucleus of housing with farmland surrounding these sites. Villages are classified through containing a built-up patch area of 0.2km²", strong("and"), "a cropland patch area of less than 0.75km²."),
+                            p("Group 1 currently contains three clusters 1A, 1B and 1C. The characteristics of group 1 villages are shown on the plots below.")
+                        )
+                    ),
+                    fluidRow(
+                        plotOutput("group_1_params")
+                    ))
+            
+        )
+    )
+    
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    values <- reactiveValues(sites_final_filtered = site_df)
+    
+    observe({
+        values$sites_final_filtered = site_df %>%
+            filter(between(travel_time, input$TravTime[1], input$TravTime[2])) %>%
+            filter(between(distance_to_road, input$RoadDist[1], input$RoadDist[2])) %>%
+            filter(between(builtup_percent, input$BuiltUpSelect[1], input$BuiltUpSelect[2])) %>%
+            filter(between(num_buildings, input$NumBuild[1], input$NumBuild[2])) %>%
+            filter(between(treecover_percent, input$TreeSelect[1], input$TreeSelect[2])) %>%
+            filter(between(cropland_percent, input$CropSelect[1], input$CropSelect[2])) %>%
+            filter(between(grassland_percent, input$GrassSelect[1], input$GrassSelect[2])) %>%
+            filter(between(shrubland_percent, input$ShrubSelect[1], input$ShrubSelect[2])) %>%
+            filter(between(lassa_occ, input$LassaSelect[1], input$LassaSelect[2]))
+    })
+
+    labels <- reactive({
+        paste0("<b>", values$sites_final_filtered$village,"</b>", "<br/>", 
+               "Cluster: ", if_else(is.na(values$sites_final_filtered$cluster), "N/A", values$sites_final_filtered$cluster), "<br/>",  
+               "Built-up: ", values$sites_final_filtered$builtup_percent*100, "%", "<br/>",
+               "Buildings: ", values$sites_final_filtered$num_buildings, "<br/>",
+               "Tree Cover: ", values$sites_final_filtered$treecover_percent*100, "%", "<br/>",
+               "Cropland: ", values$sites_final_filtered$cropland_percent*100, "%", "<br/>",
+               "Grassland: ", values$sites_final_filtered$grassland_percent*100, "%", "<br/>",
+               "Shrublandland: ", values$sites_final_filtered$shrubland_percent*100, "%", "<br/>",
+               "Lassa occ: ", round(values$sites_final_filtered$lassa_occ, 2))%>%
+            lapply(htmltools::HTML)
+    })
+    
+    output$sitemap <- renderLeaflet({ 
+        
+        pal <- colorFactor(palette = c("blue", "red", "orange"), values$sites_final_filtered$source)
+        cluster_pal <- colorFactor(palette = "viridis", values$sites_final_filtered$cluster, na.color = "white")
+        
+        bounds <- values$sites_final_filtered %>% 
+            st_bbox() %>% 
+            as.character()
+        
+        leaflet(values$sites_final_filtered) %>%
+            addProviderTiles('Esri.WorldImagery', group = "Satellite") %>%
+            addProviderTiles("OpenStreetMap.Mapnik", group = "OSM") %>%
+            addRasterImage(lc, opacity = 0.8, group = "Landuse raster") %>%
+            addPolylines(data = adm_0 %>%
+                             as.lines(),
+                         weight = 1,
+                         color = "white",
+                         popup = c("Nigerian border"),
+                         group = "Nigeria border") %>%
+            addPolylines(data = adm_1 %>%
+                             as.lines(),
+                         weight = 2,
+                         color = "white",
+                         popup = c("Administrative level 1 borders"),
+                         group = "Admin level 1") %>%
+            addPolygons(data = adm_2,
+                        weight = 2,
+                        color = "gray",
+                        fill = FALSE,
+                        popup = ~ paste0("Local Authority: ", adm_2$NAME_2),
+                        group = "Admin level 2") %>%
+            addCircleMarkers(data = values$sites_final_filtered,
+                             fillColor = ~cluster_pal(values$sites_final_filtered$cluster),
+                             radius = 5,
+                             stroke = TRUE,
+                             weight = 0.5,
+                             color = "black",
+                             fillOpacity = 0.9,
+                             popup = labels()) %>%
+            addLayersControl(baseGroups = c("Satellite", "OSM"),
+                             overlayGroups = c("Landuse raster", "Nigeria border", "Admin level 1", "Admin level 2")) %>%
+            addLegend(pal = cluster_pal,
+                      values = values$sites_final_filtered$cluster,
+                      title = "Cluster") %>%
+            hideGroup(c("Landuse raster", "Nigeria border", "Admin level 1", "Admin level 2")) %>%
+            fitBounds(bounds[1], bounds[2], bounds[3], bounds[4]) %>%
+            addScaleBar()
+        
+    })
+    
+    output$group_1_params <- renderPlot({
+        group_1_plot <- site_df %>%
+            tibble() %>%
+            select(-geometry) %>%
+            mutate(group_1 = case_when(group == 1 ~ TRUE,
+                                       TRUE ~ FALSE)) %>%
+            select(contains(c("village_id", "village", "cluster", "group_1", "num_buildings", "patch", "perc"))) %>%
+            pivot_longer(cols = !contains(c("village", "cluster", "group_1")), names_to = "metric", values_to = "value") %>%
+            mutate(metric = case_when(str_detect(metric, "patch") ~ str_to_sentence(paste(str_split(metric, "_", simplify = TRUE)[, 1], "km^2^")),
+                                      str_detect(metric, "percent") ~ str_to_sentence(paste(str_split(metric, "_", simplify = TRUE)[, 1], "%")),
+                                      str_detect(metric, "buildings") ~ paste("Buildings (n)"),
+                                      TRUE ~ "error")) %>%
+            filter(!str_detect(metric, "Landscape")) %>%
+            mutate(metric = factor(metric, levels = c("Builtup %", "Builtup km^2^", "Cropland %", "Cropland km^2^", "Grassland %", "Grassland km^2^",
+                                                      "Shrubland %", "Shrubland km^2^", "Treecover %", "Treecover km^2^", "Buildings (n)"))) %>%
+            group_by(metric) %>%
+            group_split() %>%
+            lapply(., function(x) {
+                
+                ggplot(x) +
+                    geom_point(aes(x = value, y = group_1), alpha = 0.3, position = position_jitter(height = 0.6, width = 0, seed = 123)) +
+                    labs(title = unique(x$metric),
+                         y = "Group 1",
+                         x = if(str_detect(unique(x$metric), "%")) "Proportion" else if(str_detect(unique(x$metric), "Area")) "Area" else "N") +
+                    theme_bw() +
+                    theme(legend.position = "none")
+                
+            })
+        
+        plot_grid(plotlist = group_1_plot,
+                  ncol = 2)
+            
+    })
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
